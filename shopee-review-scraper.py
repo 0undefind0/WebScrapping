0f3eaea051d4776046dbca8e70ecd90a6b046768
martin (macOS) <steam.martinjuan@gmail.com>
2023-02-22 12:14:54 +0800

    changed test sites, added next button click

--- shopee-review-scraper.py
+++ shopee-review-scraper.py
@@ -1,8 +1,12 @@
 import time
-import bs4
-from selenium import webdriver
+import bs4 #easier in extracting html elements
+from selenium import webdriver #for dynamic pages and interactions like clicking and waiting
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-all_links_list = ["https://shopee.ph//CK-ETERNITY-EDT-100ML-i.250700043.19173102207?sp_atk=84080359-385e-43bf-9660-649247a92a6d&xptdk=84080359-385e-43bf-9660-649247a92a6d", "https://shopee.ph//P125-each-minimum-of-10pcs-85ml-RMG-Perfume-(free-hi-quality-multi-purpose-pouch-every-10pcs-85ml)-i.295927972.3846392077?sp_atk=ac3d6302-1d3a-4346-a93c-329a00fb64b2&xptdk=ac3d6302-1d3a-4346-a93c-329a00fb64b2"]
+# all_links_list = ["https://shopee.ph//CK-ETERNITY-EDT-100ML-i.250700043.19173102207?sp_atk=84080359-385e-43bf-9660-649247a92a6d&xptdk=84080359-385e-43bf-9660-649247a92a6d", "https://shopee.ph//AIKEDA-Scents-Long-lasting-inspired-perfume-Premium-Oil-Based-perfume-for-women-i.423471202.23919964850?sp_atk=ef85e17f-24a3-46b4-8bc1-079518e8d7ca&xptdk=ef85e17f-24a3-46b4-8bc1-079518e8d7ca"]
+all_links_list = ["https://shopee.ph//AIKEDA-Scents-Long-lasting-inspired-perfume-Premium-Oil-Based-perfume-for-women-i.423471202.23919964850?sp_atk=ef85e17f-24a3-46b4-8bc1-079518e8d7ca&xptdk=ef85e17f-24a3-46b4-8bc1-079518e8d7ca"]
 
 first_reviews = []
 all_reviews = [] #JSON with structure [{producturl: "https://blabla", reviews: [{name: "as****ddd", stars: 5, date, comment}] }]
@@ -11,14 +15,16 @@ driver = webdriver.Chrome()
 
 for link in all_links_list:
     driver.get(link)
-    time.sleep(4)
+    driver.execute_script("document.body.style.zoom='50%'")
+    time.sleep(7)
     data = driver.page_source
     soup = bs4.BeautifulSoup(data, features="html.parser")
 
     product_url = driver.current_url
 
     # find product ratings section
-    comments = soup.find_all('div', {'class':'shopee-product-comment-list'})
+    ratinglist = soup.find_all('div', {'class':'product-ratings__list'})
+    comments = ratinglist[0].find('div', {'class':'shopee-product-comment-list'})
     time.sleep(3)
     for element in comments:
         # Each element is a Review Card consisting of (reviewer pfp, reviewer censord name, stars, time/date and variation of product, optional comment, optional media)
@@ -36,7 +42,12 @@ for link in all_links_list:
         else: first_reviews.append('None')
         print("NEXT REVIEWER!!!")
 
-    
+    # Go next page if there is one
+    nextbtn = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.shopee-page-controller.product-ratings__page-controller > button.shopee-icon-button.shopee-icon-button--right")))
+    driver.execute_script("arguments[0].click();", nextbtn)
+    print("\n\n\nClicked!\n")
+
+    time.sleep(2)
 
 with open('first_reviews.txt', 'w' , encoding="utf-8") as f:
     print(first_reviews, file=f)
